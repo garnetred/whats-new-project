601e7e28a0a17ecfe5f6fa55c74e2f0416cb8ba0
December G <dgarnetsmith@gmail.com>
2020-05-08 01:02:05 -0600

    Updates

---
+++ precache-manifest.0c1a60ac9653cab34aea860e5a6a48fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb22fa147b3436054df40fae2cfc57b",
+    "url": "/whats-new-project/index.html"
+  },
+  {
+    "revision": "9c11f3aa5ae5f7a9ccd5",
+    "url": "/whats-new-project/static/css/main.8bc6e3cc.chunk.css"
+  },
+  {
+    "revision": "9cb6184a6fce5e608afe",
+    "url": "/whats-new-project/static/js/2.2a8b2e1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whats-new-project/static/js/2.2a8b2e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c11f3aa5ae5f7a9ccd5",
+    "url": "/whats-new-project/static/js/main.969b453e.chunk.js"
+  },
+  {
+    "revision": "aa8015a7feea758ee1d7",
+    "url": "/whats-new-project/static/js/runtime-main.9bd32ac3.js"
+  }
+]);
